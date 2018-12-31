e3c30173b1ed882f6f6b3ca4f15855c5c9b18b56
russeltixguru <russel@tixguru.co>
2018-12-31 22:11:59 +0800

    update redis url connection

--- src/redis/index.js
+++ src/redis/index.js
@@ -3,6 +3,7 @@ const P = require('bluebird');
 
 P.promisifyAll(redis);
 
-const client = redis.createClient();
+const client = process.env.REDIS_URL ? 
+  redis.createClient(process.env.REDIS_URL) : redis.createClient();
 
 module.exports = client;
